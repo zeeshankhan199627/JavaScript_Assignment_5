2775cbd1452c9f9a719b2fdb6317288693a6de88
zeeshankhan199627 <64377231+zeeshankhan199627@users.noreply.github.com>
2020-06-15 23:53:32 +0500

    JavaScript Assignment Repo File has been uploaded

---
+++ api.js
@@ -0,0 +1,411 @@
+// document.write("Hellow World<br>");
+
+// document.write("Hellow Zeeshan Khan<br>");
+
+// var x = "Zeeshan Khan ";
+// var c = 199527;
+// document.write(x+c);
+
+// for(var a = 1  ; a <= 100 ;   a = a+10 ){
+// for(var b = a ; b < a+10 ; b++){
+// document.write( b + " " )
+
+//  }
+//    document.write("<br>")
+// }
+
+//Alert Chapter No.1 Task # 1
+//alert("Hello World")
+
+//Alert Chapter No.1 Task # 2
+//alert("Error! Please enter a valid password")
+
+//Alert Chapter No.1 Task # 3
+
+//alert("Welcome to JS Land... \n Happy Coding!")
+
+//Alert Chapter No.1 Task # 4
+//alert("Welcome to JS Land...")
+//alert("Happy Coding \n Prevent this page from creating additional dialogs.")
+
+//Alert Chapter No.1 Task # 5
+
+//console.log("Hello... I can run JS through my web browser's console")
+
+// Here is the end of Chapter No.1
+
+//Variable Chapter No.2 Task # 1
+
+//var a = "username";
+//alert(a)
+
+//Variable Chapter No.2 Task # 2
+
+//var myName = "Zeeshan Khan";
+//alert(myName)
+
+//Variable Chapter No.2 Task # 3
+
+//var title = "Hello World!";
+//alert(title)
+
+//Variable Chapter No.2 Task # 4
+
+// var myName = "John Doe";
+// var age = "15 Years old";
+// var education = "Certified Mobile Application Developement";
+// alert(myName);
+// alert(age);
+// alert(education);
+
+//Variable Chapter No.2 Task # 5
+
+//var a = "Pizza \n pizz \n piz \n pi \n p";
+//alert(a)
+
+//Variable Chapter No.2 Task # 6
+
+//var email = "My email address is";
+//var address = "shankhan199527@gmail.com";
+//var b = email + address;
+//alert(b)
+
+//Variable Chapter No.2 Task # 7
+
+//var book = "A Smarter Way to Learn JavaScript";
+//alert(book)
+
+//Variable Chapter No.2 Task # 8
+
+//document.write("Yah! I can write HTML content through JavaScript")
+
+//Variable Chapter No.2 Task # 9
+
+//var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(a)
+
+// Here is the end of Chapter no  2
+
+//Variable for Number Chapter  No.3 Task # 1
+
+//var a = "I am";
+//var b = 15;
+//var c = " Years old";
+//var d = a+b+c;
+//alert(d)
+
+//Variable for Number Chapter  No.3 Task # 2
+
+// var a = "You have visited this site ";
+// var b = 14 ;
+// var c = " times";
+// var d = a+b+c;
+// alert(d)
+
+//Variable for Number Chapter  No.3 Task # 3
+
+// var i = "My Birth Year Is ";
+// var l =  1996;
+// document.write(i+l);
+
+//Variable for Number Chapter  No.3 Task # 4
+
+// var a = "xyzClothing.com";
+// alert(a)
+// {
+//     var visitor = "<b>John Doe</b>";
+//     var product = " Ordered <b>5 T'Shirt</b>(s) on XYZ Clothing Store"
+// }
+// document.write(visitor+product)
+
+//Variable Legal Illegal Chapter  No.4 Task # 1
+
+// var a = "My Age is "
+// var b =  24;
+// var c = " Year Old";
+// document.write(a+b+c)
+
+//Variable Legal Illegal Chapter  No.4 Task # 2
+
+//Legal Variables
+
+// var a = 'Zeeshan ';
+// var lastName = 'Khan';
+// var $a = ' zeeshan';
+// var _a = ' khan'
+// var  zeeshan1 =  1996;
+// var c = a+lastName+$a+_a+zeeshan1;
+// document.write(c)
+
+//illegal Variables 
+// var 1a = "Zeeshan";
+// var Full Name = "Zeeshan Khan";
+// var if = 1996;
+// var NAME = "Khan";
+//  var 1last_name = "Khan";
+
+//Variable Legal Illegal Chapter  No.4 Task # 3
+
+// document.write("<b> Rules For Naming JS Variables</b> <br><br>");
+// var a = ["Variables names can only contain " , " numbers " , " names " , " $ " , " _" , "For Example : " , " $my_1stVariable <br>" ];
+// var b = ["Variables must begin with a letter " , " $ or _ ." , " For Example " , " $name " , " _name " , "or name <br>"];
+// var c = "variable names are case Sensitive <br> ";
+// var d = "variable names should not be JS Keywords";
+// document.write(a+b+c+d);
+
+//Math Expression Chapter No. 5 Task # 1
+
+// var a = 3;
+// var b = 5;
+// document.write(a+b);
+
+//Math Expression Chapter No. 5 Task # 2
+
+//Subtraction
+
+// var a = 3;
+// var b = 5;
+// document.write(b-a);
+
+//Multiplication
+
+// var a = 3;
+// var b = 5;
+// document.write(b*a);
+
+//Division
+
+// var a = 3;
+// var b = 5;
+// document.write(b/a);
+
+//Modulus
+
+// var a = 3;
+// var b = 5;
+// document.write(b % a);
+
+//Math Expression Chapter No. 5 Task # 3
+
+
+// document.write("Value after variable declaration is <b>5</b><br>");
+// document.write("Initial Value is <b> 5 </b><br>")
+// var a = 5;
+// var b = ++a;
+// var c = 7;
+// document.write("Value after increament is ")
+// document.write(b);
+// document.write("<br>Value after addition is")
+// document.write(b+c);
+// var d = --c+b;
+// document.write("<br>Value after decreament is ");
+// document.write(d);
+// document.write("<br>The Remainder is ")
+// var e = 3;
+// var f = d;
+// document.write(e/f);
+
+//Math Expression Chapter No. 5 Task # 4
+
+//  document.write("Total cost to buy 5 Tickets to a Movie is ")
+//  var i = 5;
+//  var o = 600;
+//  document.write(i*o);
+
+//Math Expression Chapter No. 5 Task # 5
+
+// var a = 5;
+// document.write("5 x 1 = " )
+// document.write(a*1)
+// document.write("<br>5 x 2 = " )
+// document.write(a*2)
+// document.write("<br>5 x 3 = " )
+// document.write(a*3)
+// document.write("<br>5 x 4 = " )
+// document.write(a*4)
+// document.write("<br>5 x 5 = " )
+// document.write(a*5)
+// document.write("<br>5 x 6 = " )
+// document.write(a*6)
+// document.write("<br>5 x 7 = " )
+// document.write(a*7)
+// document.write("<br>5 x 8 = " )
+// document.write(a*8)
+// document.write("<br>5 x 9 = " )
+// document.write(a*9)
+// document.write("<br>5 x 10 = " )
+// document.write(a*10)
+
+//Math Expression Chapter No. 5 Task # 6
+
+// var c = f - 32  * 5/9;
+// document.write(c)
+
+//Math Expression Chapter No. 5 Task # 7
+
+// var a = 650;
+// document.write("Price of item 1 ")
+// document.write(a)
+
+// document.write("<br>Quantity of item 1 is ")
+// var b = 3;
+// document.write(b)
+
+// var c = 100;
+// document.write("<br>Price of item 2 ")
+// document.write(c)
+
+// document.write("<br>Quantity of item 2 is ")
+// var d = 7;
+// document.write(d)
+
+// document.write("<br>Shipping Charges ")
+// var e = 100;
+// document.write(e)
+
+// document.write("<br> Total cost of your order is ")
+
+// var f = b*a;
+// var g = d*c;
+// document.write(f+g+e)
+
+//Math Expression Chapter No. 5 Task # 8
+
+// document.write("Total Marks : ")
+// var totalmarks = 980;
+// document.write(totalmarks)
+// document.write("<br> Marks Obtained : ")
+// var marks_obtained = 804;
+// document.write(marks_obtained)
+
+// marks_obtained * 100;
+
+// document.write("<br>Percentage : ")
+// document.write(marks_obtained / totalmarks)
+
+//Math Expression Chapter No. 5 Task # 9
+
+
+// var $us = 10*104.40;
+// var riyal = 25*28;
+// document.write("Total Currency in PKR ")
+// document.write(riyal+$us)
+
+//Math Expression Chapter No. 5 Task # 10
+
+// var a = 5 * 10 / 2;
+// document.write(a)
+
+//Math Expression Chapter No. 5 Task # 11
+
+// document.write("Current Year : ")
+// var a = 2020;
+// document.write(a)
+
+// document.write("<br> Birth Year :")
+// var b = 1996;
+// document.write(b)
+
+// document.write("<br> Your age is : ")
+// var c = a-b;
+// document.write(c)
+
+//Math Expression Chapter No. 6 to 9 Task # 1
+
+// var a = 10;
+// var b = a + ++a + a++ - --a - a--;
+// document.write(b);
+
+//Math Expression Chapter No. 6 to 9 Task # 2
+
+// var a = 2, b= 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result)
+
+// document.write("<br>Exaplain A is : 1 ")
+// document.write(" <br> B is : 2 ")
+// document.write("<br> Result  is : 3 ")
+
+//Math Expression Chapter No. 6 to 9 Task # 3
+
+// var a = prompt("Please Enter Your Name ")
+// document.write("Welcome to the developers world " + a)
+
+//Math Expression Chapter No. 6 to 9 Task # 4
+// 4 is'nt availble in the assignment file.
+
+//Math Expression Chapter No. 6 to 9 Task # 5
+
+// var a = prompt ("Calculate Multiplication" );
+// var b = prompt ("Multiply Into");
+// document.write(a*b);
+// document.write( 5 )
+
+// document.write("<b>Total Marks Obtained Marks Percentages</b><br>")
+// document.write("English       100         54      " ,)
+// var a = 100;
+// var b = 54;
+// var percentage = b*100/a;
+// document.write(percentage)
+
+// document.write("<br>English       100         54      " ,)
+// var a = 100;
+// var b = 54;
+// var percentage = b*100/a;
+// document.write(percentage)
+
+
+// document.write("<br>English       100         48      " ,)
+// var a = 100;
+// var b = 48;
+// var percentage = b*100/a;
+// document.write(percentage)
+
+// document.write( "<br>Total # ")
+// document.write( a= 300 )
+// document.write( b= 156 )
+// document.write( percentage= 52 )
+
+// User Input & Conditional Statement Chapter No. 9-11 Task # 1
+
+//  var  city = prompt("Write Your City Name") ;
+// if (city === "karachi"){
+// alert("Welcome To City Of Lights");
+// }
+
+// User Input & Conditional Statement Chapter No. 9-11 Task # 2
+
+
+// var  gender = prompt("Gender") ;
+// if (gender === "male"){
+// alert("Good Morning Sir");
+// }
+// else if (gender === "female"){
+//     alert("Good Morning Ma'am ")
+// }
+
+// User Input & Conditional Statement Chapter No. 9-11 Task # 3
+
+//  var  color = prompt("Color of road traffic signal") ;
+// if (color === "red"){
+// alert("Must Stop");
+// }
+// else if (color === "yellow"){
+//          alert("Ready to move ")
+// }
+// else if (color === "green")
+// {
+//     alert("Move now")
+// }
+
+// User Input & Conditional Statement Chapter No. 9-11 Task # 4
+
+// var fuel = 1 &&  prompt("Check your remaining fuel in your car");
+// if (fuel >= 1){
+//     alert("Please refill the fuel in your car")
+// }
+
+// User Input & Conditional Statement Chapter No. 9-11 Task # 5
+
+
+//Sorry for the short assignment submission due to i am not at my home. please accept it or give some times for this assignment i have passion to do something else something in my life for support my peoples..
